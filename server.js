96209782fd47caa5c493f9d103acd04457d5bb16
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-03-18 23:31:48 +0630

    update server js file

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ dotenv.config({
   path: "./.env",
 });
 
+const database = require("./src/config/database.config");
 const app = require("./app");
 const port = process.env.PORT;
 
+database.connect();
 app.listen(port, () => console.log(`Server is running on port : ${port}`));
