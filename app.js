2d4e3e444b5ad182b68066058023f604453c11c4
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-03-18 23:30:07 +0630

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const serviceRoute = require("./src/routes/serviceRoute");
 
+app.use(express.json());
 app.use(cors());
+app.use("/api/v1/services", serviceRoute);
 
 module.exports = app;
