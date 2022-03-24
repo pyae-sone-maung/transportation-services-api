0ebed2134ec3b31d26c65d70f522cf1f07e11b65
Pyae Sone Maung <68839375+pyae-sone-maung@users.noreply.github.com>
2022-03-24 23:44:57 +0630

    Delete serviceRoute.js

--- src/routes/serviceRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const serviceController = require("../controller/servicesController");
-
-router.post("/new-service", serviceController.createNewService);
-
-module.exports = router;
