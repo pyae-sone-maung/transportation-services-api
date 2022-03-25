2f16ee438ff748befdf71f2ea600aad87c9c7745
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-03-25 23:15:44 +0630

    update service controller

--- src/controller/servicesController.js
+++ src/controller/servicesController.js
@@ -5,12 +5,11 @@ const { validationResult } = require("express-validator");
 const create_newService = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ Errors: errors.array() });
     } else {
         try {
             const service = req.body;
             const data = await transportation_services.insertMany(service);
-            console.log(data);
             res.append("Location", req.originalUrl + data[0]._id);
             return res.status(201).json({
                 meta: { message: "create successfully", id: data[0]._id },
@@ -47,39 +46,35 @@ const show_allServices = async (req, res, next) => {
             links: { self: req.originalUrl },
         });
     } catch (error) {
-        return res.status(500).json({ errors: error.array() });
+        return res.status(500).json({ Errors: error.array() });
         next();
     }
 };
 
-// update by id
+// update by ID
 const update_serviceById = async (req, res, next) => {
     const id = req.params.service_id;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors });
+        return res.status(400).json({ Errors: errors });
     } else {
         try {
             const data = await transportation_services.findById(id);
             if (data === null) {
                 return res.status(404).json({ errors: "Id is not found" });
             } else {
-                transportation_services.findByIdAndUpdate(
-                    id,
-                    { $set: req.body },
-                    { multi: false, returnDocument: "after" },
-                    (err, result) => {
-                        if (err) {
-                            return res.status(500).json({ errors: err });
-                        } else {
-                            return res.status(200).json({
-                                result,
-                                links: { self: req.originalUrl },
-                            });
-                        }
-                    }
-                );
+                try {
+                    const result =
+                        await transportation_services.findByIdAndUpdate(
+                            id,
+                            { $set: req.body },
+                            { multi: false, returnDocument: "after" }
+                        );
+                    return res.status(200).json(result);
+                } catch (error) {
+                    return res.status(500).json({ errors: error });
+                }
             }
         } catch (error) {
             return res.status(500).json({ errors: error });
@@ -88,4 +83,49 @@ const update_serviceById = async (req, res, next) => {
     }
 };
 
-module.exports = { create_newService, show_allServices, update_serviceById };
+// delete service by ID
+const delete_serviceById = async (req, res, next) => {
+    const id = req.params.service_id;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ Errors: errors });
+    } else {
+        try {
+            const data = await transportation_services.findById(id);
+            if (data === null) {
+                return res.status(404).json({ errors: "Id is not found" });
+            } else {
+                await transportation_services.findByIdAndDelete(id);
+                return res.sendStatus(204);
+            }
+        } catch (error) {
+            return res.status(500).json({ Errors: error });
+            next();
+        }
+    }
+};
+
+// search service by name
+const search_serviceByName = async (req, res, next) => {
+    const searchName = req.params.service_name;
+    try {
+        const result = await transportation_services.find({ name: searchName });
+        return res
+            .status(200)
+            .json({
+                meta: { search: searchName, total: result.length },
+                result,
+                links: { self: req.originalUrl },
+            });
+    } catch (error) {
+        return res.status(500).json({ Errors: error });
+    }
+};
+
+module.exports = {
+    create_newService,
+    show_allServices,
+    update_serviceById,
+    delete_serviceById,
+    search_serviceByName,
+};
