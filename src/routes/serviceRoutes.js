93aa55b2cc19a492e52fc2d71f08873abc3718a8
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-03-29 23:41:52 +0630

    change routes parameter

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -2,15 +2,30 @@ const express = require("express");
 const router = express.Router();
 const serviceController = require("../controller/servicesController");
 const { validateID, validateServiceData } = require("../validation/validator");
+const { upload } = require("../utils/files-upload");
 
-router.post("/", validateServiceData, serviceController.create_newService);
+// router.post("/", validateServiceData, serviceController.create_newService);
+
+router.post("/", upload.single("image"), serviceController.imageupload);
 
 router.get("/", serviceController.show_allServices);
 
-router.patch("/:service_id", validateID, serviceController.update_serviceById);
+router.patch(
+    "/service-id/:id",
+    validateID,
+    serviceController.update_serviceById
+);
 
-router.delete("/:service_id", validateID, serviceController.delete_serviceById);
+router.delete(
+    "/service-id/:id",
+    validateID,
+    serviceController.delete_serviceById
+);
 
-router.get("/:service_name", serviceController.search_serviceByName);
+router.get("/service-id/:id", validateID, serviceController.search_serviceById);
+
+router.get("/name/:service_name", serviceController.search_serviceByName);
+
+router.get("/route/:route_name", serviceController.search_serviceByRoute);
 
 module.exports = router;
