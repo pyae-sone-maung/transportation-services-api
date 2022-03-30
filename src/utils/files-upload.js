ec6e2485fef9f21920c57fe7be58c80e0f21f2f2
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-03-31 00:27:45 +0630

    update upload middleware

--- src/utils/files-upload.js
+++ src/utils/files-upload.js
@@ -1,11 +1,37 @@
+const res = require("express/lib/response");
 const multer = require("multer");
-
 const Storage = multer.diskStorage({
-    destination: "./uploads",
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
     filename: (req, file, cb) => {
         cb(null, file.originalname + "-" + Date.now());
     },
 });
-const upload = multer({ storage: Storage });
 
-module.exports = { upload };
+const upload = multer({
+    storage: Storage,
+    limits: { fileSize: 1 * 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+            cb(
+                new Error(
+                    "Invalid file type.Only jpg, png, and jpeg format are allowed"
+                )
+            );
+        } else {
+            cb(null, true);
+        }
+    },
+}).single("image"); // image is field name
+
+const imageUpload = (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({ Error: err.message });
+        }
+    });
+    next();
+};
+
+module.exports = { imageUpload };
