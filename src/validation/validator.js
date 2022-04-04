992bc0d9217b070c089d6a6f5754007d003c5477
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-04-04 23:53:42 +0630

    modify validatate data

--- src/validation/validator.js
+++ src/validation/validator.js
@@ -1,24 +1,24 @@
 const service = require("../models/transportation_serveces");
 
-const { param, body } = require("express-validator");
+const { param, check } = require("express-validator");
 
 const validateID = [param("id").isMongoId().withMessage("Invalid ID")];
 
 const validateServiceData = [
-    body("name").not().isEmpty().trim().withMessage("Name is required."),
-    body("service")
+    check("name").not().isEmpty().trim().withMessage("Name is required."),
+    check("service")
         .not()
         .isEmpty()
         .trim()
         .withMessage("What kind of service is required"),
-    body("routes").not().isEmpty().withMessage("Transport route is required."),
-    body("vehical_type")
+    check("routes").not().isEmpty().withMessage("Transport route is required."),
+    check("vehical_type")
         .not()
         .isEmpty()
         .trim()
         .withMessage("What kind of vehical is required."),
-    body("phone").not().isEmpty().withMessage("Phone number is required."),
-    body("address").not().isEmpty().withMessage("Address is required."),
+    check("phone").not().isEmpty().withMessage("Phone number is required."),
+    check("address").not().isEmpty().withMessage("Address is required."),
 ];
 
 module.exports = { validateID, validateServiceData };
