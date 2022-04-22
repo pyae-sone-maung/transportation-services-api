77644d69b9fa6250130c23e66b75d3d949f99c34
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-04-23 00:09:58 +0630

    modify database url

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-const database = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
+const database = process.env.DATABASE_URL;
 
 exports.connect = () => {
-  mongoose
-    .connect(database, { useNewUrlParser: true })
-    .then(() => console.log("Database connected successfully."))
-    .catch((err) => {
-      console.log("Database connection fail.");
-      console.log(err);
-    });
+    mongoose
+        .connect(database, { useNewUrlParser: true })
+        .then(() => console.log("Database connected successfully."))
+        .catch((err) => {
+            console.log("Database connection fail.");
+            console.log(err);
+        });
 };
